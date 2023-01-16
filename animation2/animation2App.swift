0481a9cfc75aed42c2a759786ecb6cbacdc4e151
oniszczak <aleks42@gmail.com>
2023-01-15 19:54:46 -0500

    Initial Commit

---
+++ animation2/animation2App.swift
@@ -0,0 +1,17 @@
+//
+//  animation2App.swift
+//  animation2
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct animation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
